7224cea1086edcfc969496820b5221d2807aa55d
vedantshetti <vedantshetti123456@gmail.com>
2025-06-07 15:08:36 +0530

    made the initial setup for the frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+import Signup from './components/Signup';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<HomePage />
+  },
+  {
+    path:'/register',
+    element:<Signup/>
+  },
+  {
+    path:'/login',
+    element:<Login/>
+  }
+])
+  
+
+
 function App() {
   return (
     <div className='App'>
-      Lets build a chat application
-      <button className="btn btn-soft btn-accent">Accent</button> 
-
+      <RouterProvider router={router }/>
     </div>
   );
 }
